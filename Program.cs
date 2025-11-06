b5fb77a2629150adf3e0707744d47a956111a91e
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-11-06 15:35:44 +0100

    Template

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FlowControl;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            PrintMenu();
+            string input = Console.ReadLine() ?? string.Empty;
+            switch (input)
+            {
+                case "0":
+                    return;
+                default:
+                    PrintError("Invalid option.");
+                    break;
+            }
+        }
+    }
+
+    static void PrintMenu()
+    {
+        Console.WriteLine("Main menu:");
+        Console.WriteLine("0. Exit");
+    }
+
+    static void PrintError(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}
