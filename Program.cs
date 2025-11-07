cd128d60628055d3cd21f50f9f6974a5811dce07
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-11-07 08:54:31 +0100

    Group price

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ internal class Program
     {
         public const string Exit = "0";
         public const string Cinema = "1";
+        public const string CinemaGroup = "2";
     }
 
     static void Main(string[] args)
@@ -20,16 +21,46 @@ internal class Program
                 case MenuOption.Exit:
                     return;
                 case MenuOption.Cinema:
+                {
                     Console.WriteLine("Ange ålder:");
                     if (int.TryParse(Console.ReadLine()!, out int age))
                     {
-                        Console.WriteLine(CinemaPrice(age));
+                        Console.WriteLine(CinemaPrice(age, out _));
                     }
                     else
                     {
                         goto default;
                     }
                     break;
+                }
+                case MenuOption.CinemaGroup:
+                {
+                    Console.WriteLine("Ange antal personer:");
+                    if (int.TryParse(Console.ReadLine()!, out int count) && count > 0)
+                    {
+                        int totalPrice = 0;
+                        for (int i = 1; i <= count; i++)
+                        {
+                            Console.WriteLine($"Ange ålder för person {i}:");
+                            if (int.TryParse(Console.ReadLine()!, out int age))
+                            {
+                                CinemaPrice(age, out int price);
+                                totalPrice += price;
+                            }
+                            else
+                            {
+                                goto default;
+                            }
+                        }
+                        Console.WriteLine($"Antal personer: {count}");
+                        Console.WriteLine($"Totalpris för gruppen: {totalPrice}kr");
+                    }
+                    else
+                    {
+                        goto default;
+                    }
+                    break;
+                }
                 default:
                     PrintError("Ogiltigt val.");
                     break;
@@ -40,7 +71,8 @@ internal class Program
     static void PrintMenu()
     {
         Console.WriteLine("Huvudmeny:");
-        Console.WriteLine($"{MenuOption.Cinema}. Biopris");
+        Console.WriteLine($"{MenuOption.Cinema}. Biopris (person)");
+        Console.WriteLine($"{MenuOption.CinemaGroup}. Biopris (grupp)");
         Console.WriteLine($"{MenuOption.Exit}. Avsluta");
     }
 
@@ -51,10 +83,12 @@ internal class Program
         Console.ResetColor();
     }
 
-    static string CinemaPrice(int age)
-    {
-        return CinemaPrice(age, out _);
-    }
+    /// <summary>
+    /// Returns the cinema price based on age.
+    /// </summary>
+    /// <param name="age"></param>
+    /// <param name="price">Ticket price out</param>
+    /// <returns>String description of price</returns>
     static string CinemaPrice(int age, out int price)
     {
         if (age < 20)
